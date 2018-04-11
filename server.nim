f1ce63b5f257eba8350fc5455b84cfa0da77b1cc
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-10 22:07:12 -0500

    Return json instead of plain text

--- server.nim
+++ server.nim
@@ -3,6 +3,8 @@ import json
 
 var server = newAsyncHttpServer()
 proc cb(req: Request) {.async.} =
-  await req.respond(Http200, "你好世界！")
+  let data = %* {"msg": "你好世界！", "lucky_number": 888, "weather": "rain"}
+  let headers = newHttpHeaders([("Content-Type","application/json")])
+  await req.respond(Http200, $data, headers)
 
 waitFor server.serve(Port(8000), cb)
