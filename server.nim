962832e6c1ec305990cbe7e665568881336d46a4
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-10 22:38:39 -0500

    Pause a brief moment before sending response

--- server.nim
+++ server.nim
@@ -1,10 +1,21 @@
+import random
 import asynchttpserver, asyncdispatch
 import json
 
+randomize()
+
+const weatherChoices = @["rain", "sun", "snow", "hail"]
+
 var server = newAsyncHttpServer()
 proc cb(req: Request) {.async.} =
-  let data = %* {"msg": "你好世界！", "lucky_number": 888, "weather": "rain"}
+  let data = %* {"message": "你好世界！",
+                 "lucky_number": rand 666..888,
+                 "weather": weatherChoices.rand()}
   let headers = newHttpHeaders([("Content-Type","application/json")])
+
+  echo "Pause a bit like we are doing some intense computation"
+  await sleepAsync 2500
+
   await req.respond(Http200, $data, headers)
 
 waitFor server.serve(Port(8000), cb)
