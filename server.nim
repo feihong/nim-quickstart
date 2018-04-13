9c81615ace7be3292633b8e27e6d19cc85b2155e
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-12 21:32:07 -0500

    Refactor how the Port value gets computed

--- server.nim
+++ server.nim
@@ -7,17 +7,18 @@ randomize()
 const weatherChoices = ["rain", "sun", "snow", "hail"]
 
 proc cb(req: Request) {.async.} =
+  # echo req.url.path
   case req.url.path
   of "/json", "/json/":
     let data = %* {"message": "你好世界！",
                    "lucky_number": rand 666..888,
                    "weather": weatherChoices.rand()}
-    let headers = newHttpHeaders([("Content-Type","application/json")])
+    let headers = newHttpHeaders([("Content-Type", "application/json")])
     await req.respond(Http200, $data, headers)
   else:
     let num = rand 0x4e00..0x9fff
     let hanzi = toUTF8(Rune(num))
-    let timestr = format(now(), "MMMM d, yyyy h:mm t")
+    let timestr = times.format(now(), "MMMM d, yyyy h:mm tt")
     let html = fmt"""
     <!DOCTYPE html>
     <html>
@@ -33,6 +34,6 @@ proc cb(req: Request) {.async.} =
     await req.respond(Http200, html)
 
 let cfg = loadConfig("server.ini")
-let port = Port(cfg.getSectionValue("", "port").parseUInt)
+let port = cfg.getSectionValue("", "port").parseUInt.Port
 let server = newAsyncHttpServer()
 waitFor server.serve(port, cb)
