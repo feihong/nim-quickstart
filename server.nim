453a2fc1a6a6f136d467a006d5b4d4e4faff2313
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-12 22:47:19 -0500

    Accept query string param that varies the number of hanzi generated

--- server.nim
+++ server.nim
@@ -1,10 +1,27 @@
 import
-  random, strformat, unicode, parsecfg, strutils, times, json
-import asynchttpserver, asyncdispatch
+  random, strformat, unicode, parsecfg, strutils, sequtils, times, json, tables
+import asynchttpserver, asyncdispatch, cgi
 
 randomize()
 
 const weatherChoices = ["rain", "sun", "snow", "hail"]
+
+proc getParams(query: string) : Table[string, string] =
+  result = {"count": "1"}.toTable
+  for k, v in cgi.decodeData(query):
+    result[k] = v
+
+proc randomHanzi() : string =
+  let num = rand 0x4e00..0x9fff
+  toUTF8(Rune(num))
+
+proc hanziString(count : int) : string =
+  let posCount = if count < 1: 1
+                 else: count
+  var hanziSeq = newSeq[string](posCount)
+  for i in 0..<posCount:
+    hanziSeq[i] = randomHanzi()
+  join(hanziSeq, ", ")
 
 proc cb(req: Request) {.async.} =
   # echo req.url.path
@@ -16,9 +33,10 @@ proc cb(req: Request) {.async.} =
     let headers = newHttpHeaders([("Content-Type", "application/json")])
     await req.respond(Http200, $data, headers)
   else:
-    let num = rand 0x4e00..0x9fff
-    let hanzi = toUTF8(Rune(num))
-    let timestr = times.format(now(), "MMMM d, yyyy h:mm tt")
+    let params = getParams(req.url.query)
+    let count = try: params["count"].parseInt
+                except: 1
+    let timestr = times.format(now(), "MMMM d, yyyy h:mm:ss tt")
     let html = fmt"""
     <!DOCTYPE html>
     <html>
@@ -26,8 +44,8 @@ proc cb(req: Request) {.async.} =
         <meta charset="utf-8">
       </head>
       <body>
-      <div style="font-size: 6rem">{hanzi}</div>
-      <p>Time: {timestr}</p>
+      <p>Generated: {timestr}</p>
+      <div style="font-size: 6rem">{hanziString(count)}</div>
       </body>
     </html>
     """
