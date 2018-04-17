4a08b4a5418b5d173a0b8457490b527effebfc50
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-16 21:38:35 -0500

    Use a route pattern

--- webapp.nim
+++ webapp.nim
@@ -1,10 +1,28 @@
-import jester, asyncdispatch, htmlgen
+import random, unicode, future, strutils, strformat
+import asyncdispatch
+import jester
 
+proc randomHanzi(): string =
+  let num = rand 0x4e00..0x9fff
+  toUTF8(Rune(num))
 
-let settings = newSettings(port = Port(8000))
+proc hanziString(count: int): string =
+  let posCount = if count < 1: 1
+                 else: count
+  let hanziSeq = lc[randomHanzi() | (x <- 1..posCount), string]
+  join(hanziSeq, ", ")
+
+settings:
+  port = Port(8000)
 
 routes:
-  get "/":
-    resp h1("Hello world")
+  get "/@count?":
+    let count = if @"count" == "":
+                  5
+                else:
+                  parseInt(@"count")
+    resp fmt"""
+      <meta charset="utf-8"><h1>{hanziString(count)}</h1>
+    """
 
 runForever()
